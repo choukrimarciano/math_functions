e4e7c9605243d1ec8f55ef54e6ddcf811b6fd476
choukrimarciano <choukrimarciano@gmail.com>
2025-03-13 10:30:59 +0000

    Add 7.js

---
+++ src/7.js
@@ -0,0 +1,7 @@
+// This is an example of a simple mathematical function that takes two numbers as input and returns their sum.
+function add(a, b) {
+  return a + b;
+}
+
+// You can call this function like so:
+console.log(add(2, 3)); // Output: 5
