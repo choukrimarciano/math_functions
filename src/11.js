b37109554f84e94635b5d629a2dfb5c570528fc6
choukrimarciano <choukrimarciano@gmail.com>
2025-03-17 05:53:43 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,13 @@
+// Calculate the factorial of a number
+function factorial(n) {
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+// Calculate the sum of two numbers
+function sum(a, b) {
+  return a + b;
+}
