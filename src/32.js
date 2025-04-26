23e3092bcffb83a1a20c558b080464a588945a13
choukrimarciano <choukrimarciano@gmail.com>
2025-04-26 04:19:38 +0000

    Add 32.js

---
+++ src/32.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error('Divide by zero error');
+    }
+    return a / b;
+}
+
+function power(base, exponent) {
+    return Math.pow(base, exponent);
+}
