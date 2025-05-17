13b003d60cf519dc06faca63371d99548f01c5ae
choukrimarciano <choukrimarciano@gmail.com>
2025-05-17 09:13:47 +0000

    Add 35.js

---
+++ src/35.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Division by zero is not allowed.');
+  }
+  return a / b;
+}
+
+function square(x) {
+  return x * x;
+}
+
+function cube(x) {
+  return x ** 3;
+}
