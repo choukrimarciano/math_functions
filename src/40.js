317e844ca4837845bf4e120b5862acd8f5c0ab00
choukrimarciano <choukrimarciano@gmail.com>
2025-05-25 17:43:27 +0000

    Add 40.js

---
+++ src/40.js
@@ -0,0 +1,6 @@
+// This is an example of a simple JavaScript function to calculate the square of a number.
+function square(number) {
+  return number * number;
+}
+
+console.log(square(5)); // Output: 25
